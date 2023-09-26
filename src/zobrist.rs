da2e74713021eb9c1198c748f3b86a019f85af9c
Lilith Hoffmann <lilith.hoffmann02@gmail.com>
2023-09-26 16:54:24 +0200

    Completed zobrist updateing infrastructure

--- src/zobrist.rs
+++ src/zobrist.rs
@@ -20,6 +20,22 @@ impl ZobristHash {
     pub fn remove_castling_right(&mut self, right: usize) {
         todo!()
     }
+
+    pub fn flip_side_to_move(&mut self) {
+        todo!()
+    }
+
+    pub fn init_side_to_move(&mut self, side: Side) {
+        todo!()
+    }
+    
+    pub fn add_en_passant_square(&mut self, square: Square) {
+        todo!()
+    }
+
+    pub fn remove_en_passant_square(&mut self, square: Square) {
+        todo!()
+    }
 }
 
 lazy_static! {
