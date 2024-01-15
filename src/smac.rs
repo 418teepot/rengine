5c9b93b283ce25aa3afc4e2059f5ff57185b67f8
Lilith Hoffmann <lilith.hoffmann02@gmail.com>
2024-01-15 03:56:05 +0100

    hi

--- src/smac.rs
+++ src/smac.rs
@@ -2,7 +2,8 @@ use std::{fs, io::Write};
 pub fn smac() -> std::io::Result<()> {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input)?;
-    let mut log_file = fs::File::open("logs/smac/test.log")?;
+    let mut log_file = fs::File::create("test.log")?;
     log_file.write_all(input.as_bytes())?;
+    println!("cost={input}");
     Ok(())
 }
